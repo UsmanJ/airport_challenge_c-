d2f95ba117701ca354384f07b495a2344a7b22fe
Usman Jamil <usman.jamil@live.co.uk>
2015-11-29 17:27:52 +0000

    added planes and airport class - planes class completed

--- airport_challenge_c/Program.cs
+++ airport_challenge_c/Program.cs
@@ -6,7 +6,77 @@ namespace airport_challenge_c
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Plane p = new Plane ();
+			p.Land ();
+			Console.WriteLine (p.flying);
+			p.isLanded ();
+			p.TakeOff ();
+			p.isLanded ();
+
+			Console.WriteLine ();
+
+			Airport a = new Airport ();
+			a.Land ();
+			a.planes ();
 		}
+	}
+
+	class Plane
+	{
+		public bool flying = true;
+
+		public void Land ()
+		{
+			if (flying == false) {
+				Console.WriteLine ("Plane is already landed");
+			} else {
+				flying = false;
+				Console.WriteLine ("Plane has landed");
+			}
+		}
+
+		public void TakeOff ()
+		{
+			if (flying == true) {
+				Console.WriteLine ("Plane is already airbourne");
+			} else {
+				flying = true;
+				Console.WriteLine ("Plane has taken off");
+			}		
+		}
+
+		public void isFlying ()
+		{
+			Console.WriteLine (flying);
+		}
+
+		public void isLanded	()
+		{
+			if (flying == false) 
+			{
+				Console.WriteLine ("Plane is currently landed");
+			} else {
+				Console.WriteLine ("Plane is currently airbourne");
+			}
+		}
+	}
+
+	class Airport
+	{
+		public int[] planes = new int[80];
+
+		public void Land ()
+		{
+			if(planes.length() >= 80)
+			{
+				Console.WriteLine ("The airport is full");
+			} else {
+				for (int plane = 0; plane <= 80; plane++)
+				{
+					planes[plane] = 1;
+				}
+			}
+		}
+
 	}
 }
